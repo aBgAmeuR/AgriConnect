a490a4a40ec786623bbd31744122e16645a90a5f
aBgAmeuR <a.josset@yahoo.com>
2023-11-21 14:43:07 +0100

    Add redirect to login page in Logout component

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -4,4 +4,6 @@ import { redirect } from 'next/navigation';
 export default async function Logout() {
   await signOut({ redirect: false, callbackUrl: "/login" });
   redirect("/login");
+
+  return null;
 }
