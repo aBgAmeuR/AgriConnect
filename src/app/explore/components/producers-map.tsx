1c0b79b4a4991e9368e6e5884735925540ada89c
aBgAmeuR <a.josset@yahoo.com>
2023-11-24 09:34:38 +0100

    Add react-leaflet library for map functionality

--- src/app/explore/components/producers-map.tsx
+++ src/app/explore/components/producers-map.tsx
@@ -1,5 +1,18 @@
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+
 export const ProducersMap = () => {
   return (
-    <div>ProducersMap</div>
+      <MapContainer center={[48.866667, 2.333333]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[48.866667, 2.333333]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
   )
 }
