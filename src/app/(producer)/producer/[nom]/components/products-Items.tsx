8d3296504ae5a7d44ac242882b3d9905994718bc
Matbobe <matcanos1@gmail.com>
2023-11-30 15:45:11 +0100

    Refactor product item component

--- src/app/(producer)/producer/[nom]/components/products-Items.tsx
+++ src/app/(producer)/producer/[nom]/components/products-Items.tsx
@@ -4,25 +4,28 @@ import { Button } from '@/components/ui/button';
 import { ShoppingCart } from 'lucide-react';
 import Image from 'next/image';
 import { env } from '@/lib/env';
+import { Card } from '@/components/ui/card';
 
 export const ProductItem = ({ product }: { product: Product }) => {
   const imageUrl = product.image.includes('http') ? product.image : `${env.NEXT_PUBLIC_API_URL}/ressource/image/${product.image}`;
   return (
-    <div className="flex flex-col justify-start items-center p-4 bg-white rounded-lg shadow border border-zinc-200 max-w-xs h-[244px]">
-      <div className="h-[108px] w-[108px] flex justify-center items-center  relative">
+    <Card className="h-[244px] w-[176px]">
+      <div className="h-[150px] w-full flex justify-center items-center  relative">
         <div className="w-full h-2/3 overflow-hidden rounded-t-lg">
           <Image src={imageUrl} alt={product.name} fill style={{ objectFit: 'cover' }} />
         </div>
       </div>
-      <h3 className="text-left w-full mt-8">{product.name}</h3>
-      <div className="mt-auto flex items-center gap-1 text-gray-500">
-        <p>
-          {product.price} €/{product.unit}
-        </p>
-        <Button className="max-w-full mx-auto">
-          <ShoppingCart />
-        </Button>
+      <div className="flex flex-col gap-2 mt-6">
+        <h3 className="text-left w-full text-base ml-3">{product.name}</h3>
+        <div className="mx-3 mt-auto flex items-center justify-between   text-gray-500">
+          <p>
+            {product.price} €/{product.unit}
+          </p>
+          <Button size={'sm'}>
+            <ShoppingCart size={16} />
+          </Button>
+        </div>
       </div>
-    </div>
+    </Card>
   );
 };
