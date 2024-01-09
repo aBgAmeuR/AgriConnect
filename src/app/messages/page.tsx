4a874176926b0e956f1452dc59988e89334c0928
aBgAmeuR <a.josset@yahoo.com>
2024-01-09 16:35:38 +0100

    fix: conversation and register role

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -5,46 +5,6 @@ import { env } from "@/lib/env"
 import { MessagesSchema, Messages, Conversations, Message, Conversation } from "./messages"
 import Page401 from "@/components/page-401"
 
-const getMessageUserId = (message: Message, currentUserId: string) => {
-  if (message.sender === currentUserId) {
-    return message.receiver
-  } else {
-    return message.sender
-  }
-}
-
-const formatConversation = (messages: Messages, currentUserId: string): Conversations => {
-  const MessagesByCorresponding: Record<string, Message[]> = {}
-  messages.forEach(message => {
-    const userId = getMessageUserId(message, currentUserId)
-    if (MessagesByCorresponding[userId]) {
-      MessagesByCorresponding[userId].push(message)
-    } else {
-      MessagesByCorresponding[userId] = [message]
-    }
-  })
-
-  Object.entries(MessagesByCorresponding).forEach(([userId, messages]) => {
-    messages.sort((a, b) => {
-      const dateA = new Date(a.date)
-      const dateB = new Date(b.date)
-      return dateA.getTime() - dateB.getTime()
-    })
-  })
-
-  const conversations: Conversations = []
-  Object.entries(MessagesByCorresponding).forEach(([userId, messages]) => {
-    conversations.push({
-      date: messages[messages.length - 1].date,
-      receiver: userId,
-      messages: messages
-    })
-  })
-
-
-  return conversations
-}
-
 export default async function MessagePage() {
   const user = await getCurrentUser()
 
@@ -52,22 +12,11 @@ export default async function MessagePage() {
     return <Page401 />
   }
 
-  const res = await fetch(env.NEXT_PUBLIC_API_URL + '/messages', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${user?.accessToken}`
-    }
-  })
-  const json = await res.json()
-  const messages: Messages = MessagesSchema.parse(json.data)
-  const conversations = formatConversation(messages, user?.id || '')
-
   return (
     <>
       <MainNavBar role={user?.role || "visitor"} />
       <main>
-        <Messagerie conversations={conversations} user={user} />
+        <Messagerie user={user} />
       </main>
     </>
   )
